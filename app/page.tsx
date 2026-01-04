0562b0e7d9763d2c716520ced0fd97dc9c7a32f8
Aiman Kandakji <kandakjiai@gmail.com>
2026-01-04 02:10:27 +0200

    fix build problem and update Readme

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Home from "@/components/home-page";
 import { getProducts } from "@/lib/products";
+import { Suspense } from "react";
 
 export default function Page() {
   const products = getProducts();
@@ -9,7 +10,9 @@ export default function Page() {
       <div className="flex justify-center items-center my-8">
         <h1 className="text-4xl bold"> Welcome to my store FET</h1>
       </div>
-      <Home products={products} />
+      <Suspense fallback={<div>Loading Filters...</div>}>
+        <Home products={products} />
+      </Suspense>
     </main>
   );
 }
